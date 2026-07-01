18ea58f76098a3343a3e1723763281cef55786d6
kiqie7 <jin72181998@gmail.com>
2026-07-01 14:54:00 +0800

    添加购物车

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,22 @@
+package com.sky.service;
+
+
+import com.sky.dto.ShoppingCartDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public interface ShoppingCartService {
+
+
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     * @return
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+
+
+}
