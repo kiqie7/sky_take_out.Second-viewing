b1889161ccef7caa3bb5e7f6bfa622c8dbff12ff
kiqie7 <jin72181998@gmail.com>
2026-07-01 15:17:30 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -2,7 +2,10 @@ package com.sky.service;
 
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 
@@ -18,5 +21,9 @@ public interface ShoppingCartService {
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
 
-
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
