58d6d8cfe4a36dab5741731cceee6a7fe40d5b9b
kiqie7 <jin72181998@gmail.com>
2026-07-02 13:44:00 +0800

    用户下单

---
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.sky.service;
+
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderSubmitVO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OrderService {
+
+
+
+    /**
+     * 用户下单
+     */
+
+    OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO);
+}
